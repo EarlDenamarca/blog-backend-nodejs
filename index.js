73671bad9f204c0010cfb97e7d39a2a790464fd1
Earl Denamarca <denamarca.earl@gmail.com>
2023-06-28 23:24:31 +0800

    place all the routes into a single file routes/index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log( 'Hello World' );
+const express = require( 'express' )
+const dotenv = require( 'dotenv' ).config()
+const port =  process.env.PORT
+
+const app = express()
+const routes = require( './routes' )
+
+app.use(routes);
+
+app.listen( port, () => console.log( `Server running in port ${port}` ) )
